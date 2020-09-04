8e539fbb1b6078b08f00760914c1b14147191774
Worldsalt3 <riliwanhazzan@gmail.com>
2020-09-04 09:54:19 +0100

    delete unsed assets

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ let lon = '';
 let lat = '';
 
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded',() => {
     if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(position => {
           lat += position.coords.latitude;
           lon += position.coords.longitude;
-          fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-            .then(reaponse => reaponse.json())
-            .then(data => {
-                // document.getElementById('data1').innerHTML = JSON.stringify(data);
-            })
+          const req = new XMLHttpRequest();
+          req.open("GET", `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`, true);
+          req.send();
+          req.onload = () => {
+            const json = JSON.parse(req.responseText);
+            const timestamp = json.current.dt;
+            const theDate = new Date(timestamp * 1000);
+            console.log(json);
+            let current_html = '';
+            current_html += `${theDate}`
+            document.getElementById('current').innerHTML = current_html;
+            let {icon} = json.current.weather[0];
+            // document.getElementById('current').innerHTML = `<img src=http://openweathermap.org/img/w/${icon}.png />`;
+            // document.getElementById('minutely').innerHTML = JSON.stringify(json.weather);
+            
+          }
+            
         });
-      }
+    }
   });
