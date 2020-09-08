af743299ccc48c87b366a1c95b224353c55ef8c1
Worldsalt3 <riliwanhazzan@gmail.com>
2020-09-08 18:23:33 +0100

    add date

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ document.addEventListener('DOMContentLoaded',() => {
             const theDate = new Date(timestamp * 1000);
             console.log(json);
             let current_html = '';
-            current_html += `${theDate}`
+            current_html += `
+            <h2>${json.timezone}</h2>
+            <p>${theDate}</p>
+            `
             document.getElementById('current').innerHTML = current_html;
             let {icon} = json.current.weather[0];
             // document.getElementById('current').innerHTML = `<img src=http://openweathermap.org/img/w/${icon}.png />`;
